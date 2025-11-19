231ed403731426510e4f4266c356d21c089175a2
Saurabh80782 <rajukumar80782@ce.du.ac.in>
2025-11-19 20:35:16 +0530

    Add files via upload
    Removed redundant comments for clarity in testbench.

--- universal_adder_subtractor_tb.v
+++ universal_adder_subtractor_tb.v
@@ -1,4 +1,3 @@
-`timescale 1ns/1ps
 module universal_adder_subtractor_tb;
 
     reg [3:0] A, B;
@@ -13,18 +12,14 @@ module universal_adder_subtractor_tb;
     initial begin
         $display("Time   M  A    B   |  Result Cout  Operation");
 
-        // Addition
-        M = 0; A = 4'b0101; B = 4'b0011; #10;
+         M = 0; A = 4'b0101; B = 4'b0011; #10;
         $display("%0dns  %b  %b  %b  |   %b      %b   (ADD)", 
                  $time, M, A, B, Result, Cout);
 
-        // Addition again
         M = 0; A = 4'b1100; B = 4'b0010; #10;
 
-        // Subtraction
         M = 1; A = 4'b1000; B = 4'b0011; #10;
 
-        // Subtraction again
         M = 1; A = 4'b0100; B = 4'b0101; #10;
 
         $finish;
