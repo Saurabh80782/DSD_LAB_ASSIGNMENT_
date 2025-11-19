71ab5c216cb061db61791b7090d5edb8218038cf
Saurabh80782 <rajukumar80782@ce.du.ac.in>
2025-11-19 20:07:31 +0530

    Clean up comments in half_subtractor.v
    Removed unnecessary comments and metadata from the half_subtractor module.

--- half_subtractor.v
+++ half_subtractor.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12.11.2025 16:00:30
-// Design Name: 
-// Module Name: half_subtractor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module half_subtractor(
 input A, B,
