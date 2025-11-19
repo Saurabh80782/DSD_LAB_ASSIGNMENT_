93e065cc703b709c2b1e43460b75dfe6a4e580bc
Saurabh80782 <rajukumar80782@ce.du.ac.in>
2025-11-19 20:16:10 +0530

    Format code in full_adder.v

--- full_adder.v
+++ full_adder.v
@@ -1,11 +1,11 @@
 module full_adder(
     input  wire a,
     input  wire b,
-    input  wire cin,     // carry-in
+    input  wire cin,     
     output wire sum,
-    output wire cout     // carry-out
+    output wire cout     
 );
-    // Logic equations
-    assign sum  = a ^ b ^ cin;                 // XOR for 3 inputs
-    assign cout = (a & b) | (b & cin) | (a & cin); // Majority logic
+    
+    assign sum  = a ^ b ^ cin;                 
+    assign cout = (a & b) | (b & cin) | (a & cin); 
 endmodule
