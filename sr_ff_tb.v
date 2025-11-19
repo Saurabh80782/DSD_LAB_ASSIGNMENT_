3bc08b3d765015da6bb8641b7ed791fc923d66d4
Saurabh80782 <rajukumar80782@ce.du.ac.in>
2025-11-19 20:05:16 +0530

    Refactor SR flip-flop testbench timing statements

--- sr_ff_tb.v
+++ sr_ff_tb.v
@@ -5,19 +5,19 @@ module sr_ff_tb;
 
     sr_ff dut (.clk(clk), .s(s), .r(r), .q(q), .qbar(qbar));
 
-    always #5 clk = ~clk;  // Clock: 10ns period
+    always #5 clk = ~clk;  
 
     initial begin
         clk=0; s=0; r=0;
         $display("Time\tClk S R | Q Qbar");
         $monitor("%0dns\t%b %b %b | %b %b", $time, clk, s, r, q, qbar);
 
-        #10 s=1; r=0;   // Set
-        #10 s=0; r=0;   // Hold
-        #10 s=0; r=1;   // Reset
-        #10 s=0; r=0;   // Hold
-        #10 s=1; r=1;   // Invalid
-        #10 s=0; r=0;   // Hold
+        #10 s=1; r=0;   
+        #10 s=0; r=0;  
+        #10 s=0; r=1;   
+        #10 s=0; r=0;   
+        #10 s=1; r=1;   
+        #10 s=0; r=0;   
         #20 $finish;
     end
 endmodule
